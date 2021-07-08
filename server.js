076a7e35b6e2841a06916c5e717c57df2256068c
Ana Carolina <carolina.oliveiralima9@gmail.com>
2021-07-08 20:57:46 -0300

    add port env

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require ("./src/app")
-const PORT = 4015
+const PORT = process.env.PORT || 4015 // Mudança na porta para integração com heroku
 
 app.listen(PORT,(request,response)=>{
     console.log(`Liberdu está rodando na porta ${PORT} !!`)
